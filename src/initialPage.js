6e7596fdf9768d585f629bc999a7c208efcc8000
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-08-08 14:29:56 +0200

    modified initial page script

---
+++ src/initialPage.js
@@ -0,0 +1,38 @@
+function createBtn(id, text) {
+    const btn = document.createElement('button');
+    btn.setAttribute('id', id);
+    btn.textContent = text;
+    btn.classList.add('tab');
+    return btn;
+}
+function createMain(id) {
+    const main = document.createElement('main');
+    main.setAttribute('id', id);
+    return main;
+  }
+
+function createHeader(id){
+    const header = document.createElement('header');
+    header.setAttribute('id', id);
+
+    const nav = document.createElement('nav');
+
+    const homeBtn = createBtn('home', 'home');
+    const menuBtn = createBtn('menu', 'menu');
+    const contactBtn = createBtn('contact', 'contact');
+    nav.appendChild(homeBtn);
+    nav.appendChild(menuBtn);
+    nav.appendChild(contactBtn);
+
+    header.appendChild(nav);
+    return header;
+}
+
+function loadPage(){
+    const content = document.getElementById('content');
+
+    const header = createHeader('header');
+    content.appendChild(header);
+}
+
+export { loadPage };
